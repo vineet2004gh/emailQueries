e6b2070a4740b87d0e4f661e242626c7d378fffc
Vineet Jain <vnjain2004@Vineets-MacBook-Air.local>
2024-09-12 15:42:55 +0530

    second

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import send_test_email,contact_form
+from .views import send_test_email,contact_form,send_otp
 
 urlpatterns=[
     path('', contact_form, name='contact'), 
     path('send-email/', send_test_email, name='send_email'),
+    path('send-otp/',send_otp, name='send_otp'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -17,5 +17,17 @@ def send_test_email(request):
 
     return render(request, 'contacts/send_email_form.html', {'message': message})
 
+def send_otp(request):
+    message=""
+    if request.method=="POST":
+        email=request.POST.get('email')
+        subject='OTP'
+        message= 'The OTP is 343264'
+        email_from='vnjain2004@gmail.com'
+        recipient_list=[email]
+        send_mail(subject,message,email_from,recipient_list)
+        message = "OTP Sent!"
+    return render(request,'contacts/otp.html',{'message':message})
+
 def contact_form(request):
     return render(request, 'contacts/contact.html')
